6803ca757392e67aa7fd030fb7be454a8d6dd8a0
Rajib Deb <rajib.deb1991@gmail.com>
2021-03-16 23:54:50 +0530

    commit a new class

---
+++ src/test/java/Pages/gitHubAddNewBranch1.java
@@ -0,0 +1,12 @@
+package Pages;
+
+import org.testng.annotations.Test;
+
+public class gitHubAddNewBranch1 {
+	
+	@Test
+	public static void testGit() {
+		System.out.println("Add new in git");
+	}
+
+}
